54af1abf044a53f1459bc2d8b1ab098015e7eeaf
PlantCat1 <danielmaloney03@gmail.com>
2021-07-07 16:58:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import math
 
-cord = int(input("Cord: "))
+cord = float(input("Cord: "))
 OM1Leg = cord
-OM2Leg = 4 - cord
+OM2Leg = 1000 - cord
+# number = total # of cords
 
 print(OM1Leg)
 print(OM2Leg)
 
-OM1Distance = round(math.sqrt((OM1Leg ** 2) + 2**2), 2)
-print(OM1Distance)
+OM1Distance = round(math.sqrt((OM1Leg ** 2) + 2 ** 2), 2)
+OM2Distance = round(math.sqrt((OM2Leg ** 2) + 2 ** 2), 2)
+# pythagorean theorem, 3rd number is height, last is for rounding
+
+print("Distance to OM1: " + str(OM1Distance))
+print("Distance to OM2: " + str(OM2Distance)) 
