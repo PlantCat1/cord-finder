571d91e6ab80e3865f0ef41f4cc9e540d62ee492
PlantCat1 <danielmaloney03@gmail.com>
2021-07-07 17:45:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,12 @@ if planet == "1":
 cord = float(input("Cord: "))
 OM1Leg = cord
 OM2Leg = diameter - cord
-# number = total # of cords
 
 print(OM1Leg)
 print(OM2Leg)
 
 OM1Distance = round(math.sqrt((OM1Leg ** 2) + omheight ** 2), 2)
 OM2Distance = round(math.sqrt((OM2Leg ** 2) + omheight ** 2), 2)
-# pythagorean theorem, 3rd number is height, last is for rounding
 
 print("Distance to OM1: " + str(OM1Distance))
 print("Distance to OM2: " + str(OM2Distance)) 
