62e5ddd789c2fe5c77d6681889a2ce8fee163ae4
PlantCat1 <danielmaloney03@gmail.com>
2022-03-24 16:58:33 -0700

    sexo

--- 1D.py
+++ 1D.py
@@ -1,32 +1,35 @@
 import math
 
-def omfinder():
+def omfinder(): #finds om distances based on cords
     cord = float(input("Cord: "))
-    OM1Leg = cord
+    if cord > diameter or cord < 0: #checks cord that value is possible
+        print("This coordinate is not possible given the chosen planet.")
+        quit()
+    OM1Leg = cord #distance from edge of plane
     OM2Leg = diameter - cord
     print("OM1 Leg Distance: " + str(OM1Leg))
     print("OM2 Leg Distance: " + str(OM2Leg))
-    OM1Distance = round(math.sqrt((OM1Leg ** 2) + omheight ** 2), 2)
+    OM1Distance = round(math.sqrt((OM1Leg ** 2) + omheight ** 2), 2) #a^2 + b^2 = c^2 using current distance from edge and om heights
     OM2Distance = round(math.sqrt((OM2Leg ** 2) + omheight ** 2), 2)
     print("Distance to OM1: " + str(OM1Distance))
     print("Distance to OM2: " + str(OM2Distance))
 
-def cordfinder():
+def cordfinder(): #finds current cordinate using om distances
     OM1Distance = float(input("What is your distance to OM1? > "))
     OM2Distance = float(input("What is your distance to OM2? > "))
-    cord = round(math.sqrt((OM1Distance ** 2) - omheight ** 2), 2)
-    cord2 = round(math.sqrt((OM2Distance ** 2) - omheight ** 2), 2)
+    cord = round(math.sqrt((OM1Distance ** 2) - omheight ** 2), 2) #c^2 - a^2 = b^2
+    cord2 = round(math.sqrt((OM2Distance ** 2) - omheight ** 2), 2) #same thing but with other om distance to double check cord value
     cord2 = abs(cord2 - diameter)
-    if round(cord, 1) == round(cord2, 1):
+    if round(cord, 1) == round(cord2, 1) and cord < diameter and cord > 0:
         print("Cordinate: " + str(cord))
     else:
-        print("Please try again.")
+        print("These OMs are not possible given the chosen planet.")
         print("Cordinate: " + str(cord))
         print("Cordinate2: " + str(cord2))
 
-omorcord = input("From OMs or from cords? (1/2) ")
+omorcord = input("From current cordinates or from OM distances? (1/2) ") #asks user if they want to find OM distance from current cordinates or vice versa
 
-if omorcord == "1":
+if omorcord == "1": #to later decide which function to use
     omorcord = True
 elif omorcord == "2":
     omorcord = False
@@ -34,18 +37,21 @@ else:
     print("Please type either 1 or 2.")
     quit()
 
-planet = input(
+planet = input( #asks what body user is on so that omheight and planet diameter can be defined. will later have more planets 
 """What planet or moon are you on?
 1. Hurston
 > """)
 
-if planet == "1":
+if planet == "1": #sets these values based on the previous input
     global diameter
     diameter = 1000
     global omheight
     omheight = 213.5
+else:
+    print("Please input number representing a given body.")
+    quit()
 
-if omorcord == True:
+if omorcord == True: #calls upon which function should be used
     omfinder()
 else:
     cordfinder()
--- 2D.py
+++ 2D.py
@@ -1,22 +1,40 @@
+"""
+parent function for circle: (x - h)^2 + (y - k)^2 = r^2
+h = offset from origin in x direction
+k = offset from origin in y direction
+r = radius
+h and k are flipped (- h is offset to the right)
+r is sqaured (r of 100 only makes radius of 10)
+ 1
+4*2
+ 3
+"""
+
 import math
-a = 500
-b = 713.5
 
-cord = float(input("What is your current coordinate? > "))
+omdistance = 10 #OM distances from origin
+x = round(float(input("x cord?: ")), 2)
+y = round(float(input("y cord?: ")), 2)
 
-OM1Angle = abs(cord - 0)
-OM1Distance = round(math.sqrt(a ** 2 + b ** 2 - 2 * a * b * math.cos(math.radians(OM1Angle))), 2)
 
-OM3Angle = abs(cord - 90)
-OM3Distance = round(math.sqrt(a ** 2 + b ** 2 - 2 * a * b * math.cos(math.radians(OM3Angle))), 2)
+om1distance = round(math.sqrt((abs(x) ** 2) + abs(y - omdistance) ** 2), 2) #calculates components of distance to om and then finds hypotunese
+om2distance = round(math.sqrt((abs(x - omdistance) ** 2) + abs(y) ** 2), 2)
+om3distance = round(math.sqrt((abs(x) ** 2) + abs(y - -omdistance) ** 2), 2)
+om4distance = round(math.sqrt((abs(x - -omdistance) ** 2) + abs(y) ** 2), 2)
+centerdistance = round(math.sqrt((abs(x) ** 2) + abs(y - omdistance) ** 2), 2)
 
-OM2Angle = abs(cord - 180)
-OM2Distance = round(math.sqrt(a ** 2 + b ** 2 - 2 * a * b * math.cos(math.radians(OM2Angle))), 2)
+if centerdistance > omdistance:
+    print("\nThis coordinate is not possible given the planet radius")
 
-OM4Angle = abs(cord - 270)
-OM4Distance = round(math.sqrt(a ** 2 + b ** 2 - 2 * a * b * math.cos(math.radians(OM4Angle))), 2)
+print(
+    "\n"
+    f"om1 distance: {om1distance}\n"
+    f"om2 distance: {om2distance}\n"
+    f"om3 distance: {om3distance}\n"
+    f"om4 distance: {om4distance}\n"
+)
 
-print("OM1: " + str(OM1Distance))
-print("OM2: " + str(OM2Distance))
-print("OM3: " + str(OM3Distance))
-print("OM4: " + str(OM4Distance))
+#print("OM1: " + str(OM1Distance))
+#print("OM2: " + str(OM2Distance))
+#print("OM3: " + str(OM3Distance))
+#print("OM4: " + str(OM4Distance))
